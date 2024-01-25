cb7f82d048644d574a277c94c3eee74957e37a79
Xergan <nicolaspqd10@gmail.com>
2024-01-25 10:45:41 +0100

    home

--- verify_quiz.php
+++ verify_quiz.php
@@ -11,45 +11,29 @@ define('MDP', "/Admin1234");
 define('BDD', "xergan_geoguez");
 
 if (isset($_POST['answer'])) {
-    
+    try {
+        //On crée un objet $conn qui représente la connexion au SGBD
+        $conn = new PDO("mysql:host=" . SERVER . ";dbname=" . BDD, LOGIN, MDP);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // Assuming $user represents the current user
+        $user = $_SESSION['userinfo']['username'];
+
+        if ($_POST['answer' == ])
+        // Prepare a query to increase the user's score
+        $update = $conn->prepare("UPDATE members SET score = score + 1 WHERE user = :user");
+        $update->bindParam(':user', $user);
+        $update->execute();
+
+    } //fin du try
+    //On capture les exceptions si une exception est lancée et on affiche les informations relatives à celle-ci
+    catch (PDOException $e) {
+        echo "Erreur : " . $e->getMessage();
+    }
 
 } else {
     header('Location: quiz.php');
     exit();
 }
 
-try {
-    //On crée un objet $conn qui représente la connexion au SGBD
-    $conn = new PDO("mysql:host=" . SERVER . ";dbname=" . BDD, LOGIN, MDP);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $check = $conn->prepare("SELECT * FROM members WHERE user = :user OR email = :email");
-    $check->bindParam(':user', $user);
-    $check->bindParam(':email', $email);
-    $check->execute();
-
-    if ($check->rowCount() > 0) {
-        // L'utilisateur existe déjà
-        echo "<script>alert('L\'utilisateur existe déjà.')
-        window.location.href = 'inscription.php';</script>";
-        exit();
-    } else {
-        // L'utilisateur n'existe pas, on peut continuer l'inscription
-        $insert = $conn->prepare("INSERT INTO members(user, email, password) VALUES(:user, :email, :password, :score)");
-        $insert->bindParam(':user', $user);
-        $insert->bindParam(':email', $email);
-        $insert->bindParam(':password', $password);
-        $insert->execute();
-        $_SESSION['user'] = $user;
-        echo "<script>alert('Vous êtes inscrit.')
-        window.location.href = 'accueil.php';</script>";
-        exit();
-    }
-}
-
-//fin du try
-//On capture les exceptions si une exception est lancée et on affiche les informations relatives à celle-ci
-catch (PDOException $e) {
-    echo "Erreur : " . $e->getMessage();
-}
 ?>
